ded601cd424dd277f7daca6612ba75b582215b2c
Agoston Szepessy <agszepp@gmail.com>
2017-02-19 17:03:09 -0500

    Implemented the help command
    Help prints out the shell logo and lists the builtin commands.

--- src/builtins.c
+++ src/builtins.c
@@ -68,6 +68,52 @@ int sash_cd(struct command *cmd)
 
 int sash_help(struct command *cmd)
 {
+	printf("         ZOOOOOOOOOOOOOOOO8ZOOOOOOOOOOOO.\n");
+	printf("        ZOOOOOOOOOOOOOOOOOO8OOOOOO8OOOOO.\n");
+	printf("        OOOOOOOOOOOOOOOOO8O88OOOOONOOOOOI\n");
+	printf("       8OOOOOOOOOOOOOOO8O88D88OOOONOOOOO8.\n");
+	printf("       888D88OOOOOOOOOO8O88D88OOOO8O8OOOZ.\n");
+	printf("       ODDDDDDDDDDD888=:. .?$OO888$88OOD.\n");
+	printf("        N8888D88888888OON.  88888?IOOOO,\n");
+	printf("          =8888888888O88888D8888D,D8888.\n");
+	printf("            .N88888888888D888888:+888O:.\n");
+	printf("                $O888888D888888888O8OO.\n");
+	printf("                  .N888D88888888D88OO..\n");
+	printf("                     .D88888D88888OO8\n");
+	printf("                      D88OO8888D888O.\n");
+	printf("                    $D88,..~D88D8OO.\n");
+	printf("                  .8OO8,.  ..:?D88.\n");
+	printf("                 ?88OOO..  .,D8O8.\n");
+	printf("                N8DO8D.  ..,888ZO.\n");
+	printf("               8ODD8~..  .:NND8D.\n");
+	printf("              888O8D.  .,8N8DND.\n");
+	printf("            .7D8DDN?...,DODN88\n");
+	printf("            +DD8N8D...,8DDONO8.\n");
+	printf("           ODOD8DZ,..,8D8DD8O.\n");
+	printf("     8$ ..888N8DD:.,MDD8NDDO.\n");
+	printf("     ?O88ODNDN88~~DDDMND88D.\n");
+	printf("      O8D8DDN88++8D88N8D8D.\n");
+	printf("       888DDDN=8NDNN888DDO.\n");
+	printf("       ..DDD8I..NONDN8ONO?.\n");
+	printf("         ..DZ.  .?I88DOD8.\n");
+	printf("                   ,8D8I.\n");
+	printf("                    ....\n");
+	printf("\n");
+	printf("\n");
+	printf("         ___  __ _ ___| |__\n");
+	printf("        / __|/ _` / __| '_ \\\n");
+	printf("        \\__ \\ (_| \\__ \\ | | |\n");
+	printf("        |___/\\__,_|___/_| |_|\n");
+	printf("\n");
+	printf("sash is Szepessy Agoston's shell\n");
+	printf("If you're not sure what a command does type man command_name. If you don't know what man does, type man man.\n");
+	printf("\n");
+	printf("The following are builtin commands:\n");
+
+	for(int i = 0; i < sash_num_builtins(); ++i)
+	{
+		printf("%s\n", builtin_str[i]);
+	}
 
 	return 1;
 }
@@ -89,6 +135,8 @@ int sash_pwd(struct command *cmd)
 	path = getcwd(path, PATH_MAX + 1);
 
 	printf("%s\n", path);
+
+	free(path);
 
 	return 1;
 }
